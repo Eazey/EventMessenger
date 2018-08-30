a53aaf19de73fccced3385c0e0f978562063552d
Eazey_Company_YuChang <eazey@foxmail.com>
2018-08-30 15:55:45 +0800

    Base Implement
    Implement the basic functions of the event system

--- Assets/Core/Script/EventMessenger.cs
+++ Assets/Core/Script/EventMessenger.cs
@@ -6,19 +6,63 @@ namespace EazeyFramework
 {
     public static class EventMessenger
     {
+        public class EventIDComparer : IEqualityComparer<EventMessage.ID>
+        {
+            public bool Equals(EventMessage.ID x, EventMessage.ID y)
+            {
+                return (int)x == (int)y;
+            }
+
+            public int GetHashCode(EventMessage.ID obj)
+            {
+                return (int)obj;
+            }
+        }
+
+        private static Dictionary<EventMessage.ID, EventHandler> m_eventMap;
+
+        static EventMessenger()
+        {
+            m_eventMap = new Dictionary<EventMessage.ID, EventHandler>(new EventIDComparer());
+        }
+
         public static void AddListener(EventMessage.ID eventID, EventHandler callback)
         {
+            if (!m_eventMap.ContainsKey(eventID))
+                m_eventMap.Add(eventID, null);
 
+            m_eventMap[eventID] += callback;
         }
 
         public static void RemoveListener(EventMessage.ID eventID, EventHandler callback)
         {
-
+            if (m_eventMap.ContainsKey(eventID))
+            {
+                m_eventMap[eventID] -= callback;
+                if (m_eventMap[eventID] == null)
+                    m_eventMap.Remove(eventID);
+            }
         }
 
         public static void Broadcast(EventMessage eventMsg)
         {
+            EventMessage.ID id = eventMsg.EventID;
+            if(m_eventMap.ContainsKey(id))
+            {
+                if (m_eventMap[id] != null)
+                    m_eventMap[id](eventMsg);
+            }
+        }
 
+        public static void RemoveAllListener(EventMessage.ID eventID)
+        {
+            if (m_eventMap.ContainsKey(eventID))
+                m_eventMap.Remove(eventID);
+        }
+
+        public static void Clear()
+        {
+            m_eventMap.Clear();
         }
     }
 }
